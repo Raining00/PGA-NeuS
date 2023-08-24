29576f72c9ab9f84a5f2839427f7d256aefbe326
YuanMu Xu <1141501284@qq.com>
2023-08-24 19:23:05 +0800

    update train.py logical

--- train.py
+++ train.py
@@ -9,8 +9,18 @@ if __name__ == '__main__':
     # TODO:train static
     print('background SDF and moving object')
 
+    # TODO: extract moving object's mesh
+    # wirte a .obj file
+    # give the name of the .obj file
+    print('extract moving object\'s mesh')
+
     # TODO:initial velocity optimization
+    # methods? veloicty of each finite element
+    # read .obj file according to the name
     print('initial velocity optimization')
+
+
+
 
     #TODO:train dynamic
     print('physical parameters of moving object')
