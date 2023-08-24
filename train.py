1fd0e6639b6d012e5180581ca44426e4ee0de1fe
YuanMu Xu <1141501284@qq.com>
2023-08-24 19:18:03 +0800

    hokai impact 3, start!

---
+++ train.py
@@ -0,0 +1,16 @@
+import os
+
+
+
+if __name__ == '__main__':
+    # TODO: raed parser
+    print('config_parser')
+
+    # TODO:train static
+    print('background SDF and moving object')
+
+    # TODO:initial velocity optimization
+    print('initial velocity optimization')
+
+    #TODO:train dynamic
+    print('physical parameters of moving object')
