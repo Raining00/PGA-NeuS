f96f98421c8e198ac32e544b21df4617e678f423
xym <xuyuanmu@outlook.com>
2023-12-18 16:28:26 +0800

    fix some naming bugs

--- exp_runner.py
+++ exp_runner.py
@@ -326,7 +326,7 @@ class Runner:
                                         '{:0>8d}_{}_{}.png'.format(self.iter_step, i, idx)),
                            np.concatenate([img_fine[..., i],
                                            self.dataset.image_at(idx, resolution_level=resolution_level)]))
-            if len(depth_map) > 0:
+            if len(depth_fine) > 0:
                 cv.imwrite(os.path.join(self.base_exp_dir,
                                         'validations_fine',
                                         '{:0>8d}_{}_{}_depth.png'.format(self.iter_step, i, idx)),
